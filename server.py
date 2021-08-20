f282d43b99559bce88d52c94cb03df1bacac9a9e
cheongwonyoung <86768006+cheongwonyoung@users.noreply.github.com>
2021-08-20 16:01:49 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #Server
-from flask import Flask, request, send_file # 서버 구현을 위한 falsk 객체 import
-from flask_restx import Api, Resource # Api 구현을 위한 Api 객체 import
+from flask import Flask, request # 서버 구현을 위한 falsk 객체 import
+from flask_restx import Api # Api 구현을 위한 Api 객체 import
 import jsonpickle
 import cv2
 import pymysql
@@ -13,7 +13,6 @@ import detect
 
 
 app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
-api = Api(app)  # Flask 객체에 Api 객체 등록
 
 @app.route('/file_upload', methods=['GET', 'POST'])
 def upload_files():
