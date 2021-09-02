35279527e0a66671ce857e457bead4b0100433dd
cheongwonyoung <86768006+cheongwonyoung@users.noreply.github.com>
2021-09-02 13:29:10 +0900

    Update server.py

--- server.py
+++ server.py
@@ -14,6 +14,20 @@ import detect
 
 app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
 
+@app.route('/data_upload', methods=['GET', 'POST'])
+def upload_data():
+    if request.method == 'POST':
+       global conf
+       global iou
+       params = json.loads(request.get_data(),encoding='utf-8')
+       conf = str(params['conf'])
+       iou = str(params['iou'])
+
+       print('conf=',conf)
+       print('iou=',iou)
+
+    return "Clear" 
+
 @app.route('/file_upload', methods=['GET', 'POST'])
 def upload_files():
     if request.method == 'POST':
@@ -26,8 +40,8 @@ def upload_files():
         f.save(path)
 
         #os.system('python detect.py --weights ./runs/exp6_yolov4-csp-results/weights/best_yolov4-csp-results.pt --img 2448 --conf 0.4 --source tt22/ --device 2') # detect
-        data = subprocess.check_output(['python', 'detect.py', '--weights', './runs/exp6_yolov4-csp-results/weights/best_yolov4-csp-results.pt', '--img', '2448', '--conf', '0.4', '--source', 'tt22/', '--device', '2' ]) 
-        
+        data = subprocess.check_output(['python', 'detect.py', '--weights', './runs/exp6_yolov4-csp-results/weights/best_yolov4-csp-results.pt', '--img', '2448', '--conf', conf, '--iou', iou, '--source', 'tt22/', '--device', '0' ]) 
+        print(type(data))
         input_FilePath = 'tt22'
         if os.path.exists(input_FilePath):
             for file in os.scandir(input_FilePath):
